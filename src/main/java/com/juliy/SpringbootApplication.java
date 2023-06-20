47d17ccf23ced86d3b878ffc2c0c4bdc765ee6b8
juliy <799399654@qq.com>
2023-06-20 22:58:26 +0800

    配置开发、生产环境； 调整项目启动时的打印信息

--- src/main/java/com/juliy/SpringbootApplication.java
+++ src/main/java/com/juliy/SpringbootApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 /**
  * @author juliy
  * @date 2023/3/1 13:23
@@ -15,18 +12,19 @@ import java.net.UnknownHostException;
 @SpringBootApplication
 public class SpringbootApplication {
 
-    public static void main(String[] args) throws UnknownHostException {
+    public static void main(String[] args) {
         ConfigurableApplicationContext application = SpringApplication.run(SpringbootApplication.class, args);
+
         Environment env = application.getEnvironment();
-        String ip = InetAddress.getLocalHost().getHostAddress();
-        String port = env.getProperty("server.port");
-
-        System.out.println("----------------------------------------------------------\n\t" +
-                "Application  is running! Access URLs:\n\t" +
-                "Local访问网址: \t\thttp://localhost:" + port + "\n\t" +
-                "External访问网址: \thttp://" + ip + ":" + port + "\n\t" +
-                "文档地址: \t\t\thttp://localhost:" + port + "/doc.html\n" +
-                "----------------------------------------------------------");
+        String activeProfile = env.getProperty("spring.profiles.active");
+        // 开发环境打印信息
+        if ("dev".equals(activeProfile)) {
+            String port = env.getProperty("server.port");
+            System.out.println("----------------------------------------------------------\n\t" +
+                    "SHOKA-BLOG后端启动成功！\n\t" +
+                    "Local访问网址: \t\thttp://localhost:" + port + "\n\t" +
+                    "文档地址: \t\t\thttp://localhost:" + port + "/doc.html\n" +
+                    "----------------------------------------------------------");
+        }
     }
-
 }
